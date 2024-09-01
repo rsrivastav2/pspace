13828b794b7ef9d968fca7b5052a7f22ea0f7d96
rsrivastav2 <81876194+rsrivastav2@users.noreply.github.com>
2024-09-01 21:19:44 +0530

    Update testPush.py

--- testPush.py
+++ testPush.py
@@ -44,7 +44,7 @@ y = data['count']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Normalize/Scale features
-scaler = StandardScaler()
+scaler = MinMaxScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
