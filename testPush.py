015934655168fb4f522bcdd4a57a643b13c60f93
rsrivastav2 <81876194+rsrivastav2@users.noreply.github.com>
2024-09-15 14:29:15 +0530

    Update testPush.py

--- testPush.py
+++ testPush.py
@@ -1,17 +1,5 @@
-from kubernetes import client, config
-
-def main():
-    # Load kubeconfig from within the cluster
-    config.load_incluster_config()
-
-    # Create a Kubernetes API client
-    api_instance = client.CoreV1Api()
-
-    # List all pods in the default namespace
-    print("Listing pods with their IPs:")
-    ret = api_instance.list_namespaced_pod(namespace='default')
-    for pod in ret.items:
-        print(f"{pod.metadata.name} {pod.status.pod_ip}")
-
-if __name__ == "__main__":
-    main()
+Perf
+| where ObjectName == "Processor" and CounterName == "% Processor Time"
+| summarize avg(CounterValue) by bin(TimeGenerated, 5m), Computer
+| project TimeGenerated, Computer, avg_CounterValue
+| order by TimeGenerated asc
