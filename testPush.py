8492fe2d3462dc8fa98a81b0413b36aad4d1421f
rsrivastav2 <81876194+rsrivastav2@users.noreply.github.com>
2024-09-15 15:28:47 +0530

    Update testPush.py

--- testPush.py
+++ testPush.py
@@ -1,5 +1,5 @@
 Perf
-| where ObjectName == "Processor" and CounterName == "% Processor Time"
-| summarize avg(CounterValue) by bin(TimeGenerated, 5m), Computer
-| project TimeGenerated, Computer, avg_CounterValue
+| where ObjectName == "LogicalDisk" and CounterName in ("% Free Space", "% Disk Space Used")
+| summarize avg(CounterValue) by bin(TimeGenerated, 1h), Computer, CounterName, InstanceName
+| project TimeGenerated, Computer, InstanceName, CounterName, avg_CounterValue
 | order by TimeGenerated asc
