1f945a6d72382f025284a8683bbf77aa8bbb4a15
rsrivastav2 <81876194+rsrivastav2@users.noreply.github.com>
2025-01-23 20:14:14 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,5 @@
-import React, { useEffect, useState } from 'react';
-
-function Dropdown() {
-    const [options, setOptions] = useState([]);
-    
-    // Fetch data when the component mounts
-    useEffect(() => {
-        fetch('http://localhost:5000/api/data')
-            .then((response) => response.json())
-            .then((data) => {
-                setOptions(data); // Store the API response in state
-            })
-            .catch((error) => {
-                console.error('Error fetching data:', error);
-            });
-    }, []);
-
-    return (
-        <select>
-            {options.map((option) => (
-                <option key={option.id} value={option.id}>
-                    {option.name}
-                </option>
-            ))}
-        </select>
-    );
+.select-dropdown {
+  width: 300px;  /* Set the width of the dropdown */
+  height: 40px;  /* Set the height of the dropdown */
+  font-size: 16px; /* Increase the font size inside the dropdown */
 }
-
-export default Dropdown;
