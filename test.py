411668da9abb4d81a48a621ae271a4faf46d9689
rsrivastav2 <81876194+rsrivastav2@users.noreply.github.com>
2025-01-23 20:40:26 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
-.select-dropdown {
-  width: 300px;  /* Set the width of the dropdown */
-  height: 40px;  /* Set the height of the dropdown */
-  font-size: 16px; /* Increase the font size inside the dropdown */
-}
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    selected_value = request.args.get('selected')  # Get the selected value from the query parameters
+
+    if selected_value == 'option1':
+        data = [{'id': 1, 'name': 'Result 1'}, {'id': 2, 'name': 'Result 2'}]
+    elif selected_value == 'option2':
+        data = [{'id': 3, 'name': 'Result 3'}, {'id': 4, 'name': 'Result 4'}]
+    else:
+        data = []
+
+    return jsonify(data)
