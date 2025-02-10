60c6efb560e3ba782da30569a1d6e1e086afbdc1
rsrivastav2 <81876194+rsrivastav2@users.noreply.github.com>
2025-02-10 21:47:06 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
- 
-      setData(result);
-      setIsAuthorized(result.toLowerCase().includes("authorize")); // Check if "authorize" exists
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
+  method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
