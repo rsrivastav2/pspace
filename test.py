7ecfb6017b896087c932dccf0d884bf595b9701c
rsrivastav2 <81876194+rsrivastav2@users.noreply.github.com>
2025-06-26 21:58:41 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-python -c "from airflow.operators.python import PythonOperator; print('PythonOperator is ready')"
+set AIRFLOW_VERSION=2.9.0
+set PYTHON_VERSION=3.10
+set CONSTRAINT_URL=https://raw.githubusercontent.com/apache/airflow/constraints-%AIRFLOW_VERSION%/constraints-%PYTHON_VERSION%.txt
+
+pip install "apache-airflow==%AIRFLOW_VERSION%" --constraint %CONSTRAINT_URL%
