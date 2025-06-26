6d054a79792336f81803bec0818131dc0c6523ea
rsrivastav2 <81876194+rsrivastav2@users.noreply.github.com>
2025-06-26 09:17:34 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,56 @@
-pip install pywin32 win10toast
+Sequence - Intro(5days WFO, Weekend Support) -> Current role and responsibilties -> Monitoring tools -> Webservices -> Java/Python -> Database -> Unix -> Cloud
+
+Webserives:
+
+What is a web service?
+
+What are the types of web services?
+
+What is the difference between SOAP and REST?
+
+What is WSDL?
+
+What is UDDI?
+
+What are HTTP methods used in REST?
+
+What is the difference between PUT and POST?
+
+What is the role of XML and JSON in web services?
+
+Core Java:
+
+What are the main features of Java?
+
+What is the difference between JDK, JRE, and JVM?
+
+Why is Java platform-independent?
+
+What is the difference between == and .equals()?
+
+What is autoboxing and unboxing?
+
+What is the difference between final, finally, and finalize()?
+
+Ezxception handling?
+
+Monitoring Tools?
+
+Cloud :
+
+What is Azure Resource Manager (ARM)?
+
+What are Azure App Services?
+
+What is Azure Blob Storage vs File Storage?
+
+What is Azure DevOps used for?
+
+What is Azure Functions?
+
+How does Azure Active Directory differ from on-prem AD?
+
+What is NSG (Network Security Group)?
+
+What is the difference between Azure VM and App Service?
+
