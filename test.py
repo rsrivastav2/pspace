f8b7615b9510d04c51a1d492ea2d1dfa6404b8c8
rsrivastav2 <81876194+rsrivastav2@users.noreply.github.com>
2025-06-26 20:50:59 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,56 +1,16 @@
-Sequence - Intro(5days WFO, Weekend Support) -> Current role and responsibilties -> Monitoring tools -> Webservices -> Java/Python -> Database -> Unix -> Cloud
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
 
-Webserives:
+def my_task():
+    print("Running my task")
 
-What is a web service?
+with DAG("example_dag",
+         start_date=datetime(2024, 1, 1),
+         schedule_interval='@daily',
+         catchup=False) as dag:
 
-What are the types of web services?
-
-What is the difference between SOAP and REST?
-
-What is WSDL?
-
-What is UDDI?
-
-What are HTTP methods used in REST?
-
-What is the difference between PUT and POST?
-
-What is the role of XML and JSON in web services?
-
-Core Java:
-
-What are the main features of Java?
-
-What is the difference between JDK, JRE, and JVM?
-
-Why is Java platform-independent?
-
-What is the difference between == and .equals()?
-
-What is autoboxing and unboxing?
-
-What is the difference between final, finally, and finalize()?
-
-Ezxception handling?
-
-Monitoring Tools?
-
-Cloud :
-
-What is Azure Resource Manager (ARM)?
-
-What are Azure App Services?
-
-What is Azure Blob Storage vs File Storage?
-
-What is Azure DevOps used for?
-
-What is Azure Functions?
-
-How does Azure Active Directory differ from on-prem AD?
-
-What is NSG (Network Security Group)?
-
-What is the difference between Azure VM and App Service?
-
+    task = PythonOperator(
+        task_id="print_hello",
+        python_callable=my_task
+    )
