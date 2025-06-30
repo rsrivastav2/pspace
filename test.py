8732473fb22513f8ecafb11df3fc7f4227acb190
rsrivastav2 <81876194+rsrivastav2@users.noreply.github.com>
2025-06-30 19:18:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-# Set variables
-$serverInstance = "localhost"       # Change if needed
-$databaseName = "TestDB_PowerShell"
-
-# Create the T-SQL command
-$query = @"
-CREATE DATABASE [$databaseName];
-"@
-
-# Run the SQL command
-Invoke-Sqlcmd -ServerInstance $serverInstance -Query $query
+Start-Service -Name 'MSSQLSERVER'
