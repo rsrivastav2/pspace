27692de15bc3533592e98e7fd9ccb56b5ed26350
rsrivastav2 <81876194+rsrivastav2@users.noreply.github.com>
2025-07-02 21:30:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,14 @@
- status = "success" if exit_code == 0 else "failed"
-
-    except Exception as e:
-        print(f"[{job_name} ERROR]: {str(e)}")
-        status = "failed"
-
-    end_time = datetime.now()
-    duration = (end_time - start_time).total_seconds() / 60  # in minutes
-    return status, start_time, end_time, duration
-
-
-def update_status(conn, job_name, status, start_time, end_time, duration):
+def fetch_jobs(db_path):
+    conn = connect_db(db_path)
     cursor = conn.cursor()
-    cursor.execute("""
-        UPDATE Jobs 
-        SET status = ?, start_time = ?, end_time = ?, duration_minutes = ?
-        WHERE job_name = ?
-    """, (status, start_time, end_time, duration, job_name))
-    conn.commit()
 
-for job in execution_order:
-    status, start_time, end_time, duration = run_job(job, jobs[job])
-    update_status(conn, job, status, start_time, end_time, duration)
-    
-    if status != "success":
-        print(f"Stopping execution due to failure in {job}")
-        break
+    cursor.execute("SELECT job_name, status, start_time, end_time, duration_minutes FROM Jobs")
+
+    rows = cursor.fetchall()
+
+    print("\n=== Jobs Table ===")
+    for row in rows:
+        print(f"Job: {row.job_name}, Status: {row.status}, "
+              f"Start: {row.start_time}, End: {row.end_time}, Duration (min): {row.duration_minutes}")
+
+    conn.close()
