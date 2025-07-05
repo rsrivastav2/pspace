c65831a55d70e5e75625ec12e682ec95aa3e1dfc
rsrivastav2 <81876194+rsrivastav2@users.noreply.github.com>
2025-07-05 11:30:21 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-if job["status"] == "Held":
-        print(f"Job '{job_name}' is held and will not be executed.")
-        return
+cursor.execute("SELECT machine_name, command, status FROM Jobs WHERE job_name = ?", (job_name,))
+    row = cursor.fetchone()
+    conn.close()
+
+    if row and len(row) >= 3:
+        return {
+            "machine": row[0],
+            "command": row[1],
+            "status": row[2]
+        }
+    else:
+        print(f"Invalid or missing job: {job_name}")
+        return None
