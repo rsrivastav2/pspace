1839bd330d3a9bd78d2656e25a9758d65eac461f
rsrivastav2 <81876194+rsrivastav2@users.noreply.github.com>
2025-07-05 11:10:56 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,3 @@
-def get_job_details(job_name):
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT machine_name, command, status FROM Jobs WHERE job_name = ?", (job_name,))
-    result = cursor.fetchone()
-    conn.close()
-    
-    if result:
-        return {
-            "machine": result[0],
-            "command": result[1],
-            "status": result[2]
-        }
-    return None
+if job["status"] == "Held":
+        print(f"Job '{job_name}' is held and will not be executed.")
+        return
