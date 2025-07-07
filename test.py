0e88e8b22e6db886860d6c2fcacf1a33f00dc10d
rsrivastav2 <81876194+rsrivastav2@users.noreply.github.com>
2025-07-07 16:54:14 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-cursor.execute("""
-    INSERT INTO job_history 
-    (job_name, execution_date, start_time, end_time, duration_minutes, status, remarks)
-    VALUES (?, ?, ?, ?, ?, ?, ?)
-""", (job_name, exec_date, start_time, end_time, duration, status, remarks))
+Drag-and-drop interface to design job pipelines visually like Airflow/Dagster.
+
+Highlight critical paths and execution timelines
