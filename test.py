b2c9006a19030440ffcbf263d87a122407f539b1
rsrivastav2 <81876194+rsrivastav2@users.noreply.github.com>
2025-08-02 15:49:39 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,2 @@
-This tool, while initially designed for production support and incident response, can be extended and adapted for:
-
-Dev/Test Environments: Allow developers or QA teams to manage test pods, restart apps, or scale services without needing deep Kubernetes knowledge.
-
-Self-Service Portals: Integrate it into internal developer platforms for teams to self-serve common operations safely and securely.
-
-On-Call Operations: Make it easier for on-call engineers to take action even from mobile/tablet browsers during outages without logging into the CLI.
-
-Non-AKS Clusters: With minor backend changes, it can support EKS, GKE, or on-prem Kubernetes clusters.
-
-Governance & Compliance: The built-in audit logging feature makes it ideal for teams needing traceability in regulated environments.
-
-Training & Enablement: New engineers can safely explore K8s operations with guardrails, making onboarding and upskilling faster.
-
+SELECT * FROM your_table
+WHERE varchar_col > 100;
